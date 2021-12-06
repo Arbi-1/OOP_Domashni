9648bcdee1b1a83448f0c1c5611753f71d38d5da
Arbisjan Abazi <93103650+Arbi-1@users.noreply.github.com>
2021-12-06 17:49:59 +0100

    Add files via upload

---
+++ zadacha2/src/Glavna.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Glavna {
+
+    public static void main(String[] args){
+        List<String> footballClubs = new ArrayList<>();
+        footballClubs.add("Real Madrid");
+        footballClubs.add("Barcelona");
+        footballClubs.add("Atletico Madrid");
+System.out.println(footballClubs.size());
+
+    }
+
+
+}
